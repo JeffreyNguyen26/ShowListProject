2e3328306fe1dcc99f7ab3e2a7730263951fd3b4
Jeffrey Nguyen <jeffrey.nguyen98@gmail.com>
2021-06-02 17:35:45 +0700

    Update api_link_constant.js

--- src/service/constant/api_link_constant.js
+++ src/service/constant/api_link_constant.js
@@ -1,1 +1,2 @@
- export const NOCOCID_API = "http://toan0701.ddns.net:9080/Nococid/api/";
+// export const NOCOCID_API = "http://toan0701.ddns.net:9080/Nococid/api/";
+export const NOCOCID_API = "https://nococidapi.azurewebsites.net/api/";
